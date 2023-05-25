68f7c203df0822377142bcd7482ba3ea19abf758
JuliPerezApiwan <julieta.perezapiwan@gmail.com>
2023-05-25 05:20:14 -0300

    ok

--- client/src/components/secciones/secciones.jsx
+++ client/src/components/secciones/secciones.jsx
@@ -1,60 +1,70 @@
-const products = [
-  {
-    id: 1,
-    name: 'Sobre mi',
-    href: '#',
-    imageSrc: '',
-  },
-  {
-    id: 1,
-    name: 'Estudios',
-    href: '#',
-  },
-  {
-    id: 2,
-    name: 'Experiencias',
-    href: '#',
-  },
-  {
-    id: 3,
-    name: 'Proyectos',
-    href: '#',
-  },
-  // More products...
-]
-
-export default function Secciones () {
+export const Secciones = () => {
   return (
-    <div >
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 rounded-full  aspect-w-1 overflow-hidden  w-[18rem] bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-              <div>
-                <br/>
-                  <h1 className="text-xl text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h1>
-                </div>
-              <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover  object-center lg:h-full lg:w-full"
-                />
-                
-              </div>
-              <div className="mt-4 flex justify-between">
-                
-              </div>
-            </div>
-          ))}
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-8 lg:grid-cols-2 sm:max-w-sm sm:mx-auto lg:max-w-full">
+        <div className="p-8 bg-white border rounded shadow-sm">
+          <a
+            href="/"
+            aria-label="Article"
+            title="Jingle Bells"
+            className="inline-block mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+          >
+            Sobre mi
+          </a>
+          <p className="mb-5 text-gray-700">
+            Some pilots get picked and become television programs. Some don't,
+            become nothing.
+          </p>
+          
+        </div>
+        <div className="p-8 bg-white border rounded shadow-sm">
+        
+          <a
+            href="/"
+            aria-label="Article"
+            title="Happy new Year"
+            className="inline-block mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+          >
+            Estudios
+          </a>
+          <p className="mb-5 text-gray-700">
+            Pommy ipsum smeg head whizz morris dancers come hither, bugger
+            codswallop gob. Taking the mick middle class bog.
+          </p>
+        </div>
+        <div className="p-8 bg-white border rounded shadow-sm">
+          <a
+            href="/"
+            aria-label="Article"
+            title="Why i love C++"
+            className="inline-block mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+          >
+            Experiencias
+          </a>
+          <p className="mb-5 text-gray-700">
+            Sportacus andrew weatherall goose Refined gentlemen super mario des
+            lynam alpha trion zap rowsdower.
+          </p>
+          
+        </div>
+        <div className="p-8 bg-white border rounded shadow-sm">
+          <a
+            href="/"
+            aria-label="Article"
+            title="Why i love C++"
+            className="inline-block mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+          >
+            Proyectos
+          </a>
+          <p className="mb-5 text-gray-700">
+            Sportacus andrew weatherall goose Refined gentlemen super mario des
+            lynam alpha trion zap rowsdower.
+          </p>
+          
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
+export default Secciones
